4c67ff1206b58eb1e5a5a7fc9d99f5876509a646
manjulk <manjulkj@gmail.com>
2015-06-03 19:47:35 +0530

    test - dot net changes
    dot net

---
+++ List.js
@@ -0,0 +1,124 @@
+﻿
+function ListSelections(clientID) {
+    this.id = clientID;
+}
+
+ListSelections.prototype = {
+    init: function () {
+        var btn = $("#" + this.id + "btnAddToRight");
+        var controlId = this.id;
+        var control = this;
+
+        //right btn
+        $("#" + this.id + "btnAddToRight").click(function (e) {
+
+        });
+
+        //right btn all
+   
+                var targetLen = targetList.options.length;
+
+                for (var i = targetLen - 1; i > -1; i--) {
+                    sourceList[sourceLen++] = new Option(targetList.options[i].text, targetList.options[i].value);
+                    targetList.options.remove(i);
+                }
+
+                targetLen = targetList.options.length;
+                var hidden = $("#" + controlId + "hfTargetValues");
+                var strTarget = '';
+                for (var i = 0; i < targetLen; i++) {
+                    if (i < targetLen - 1)
+                        strTarget += targetList.options[i].value + "|";
+                    else
+                        strTarget += targetList.options[i].value
+                }
+
+                hidden.val(strTarget);
+
+                control.onRemoveComplete();
+            }
+        });
+    },
+
+    addSourceItem: function (text, value) {
+        var sourceList = document.getElementById(this.id + "lstbxtSelectionPool");
+
+        var sourceLen = sourceList.options.length;
+        sourceList[sourceLen++] = new Option(text, value);
+    },
+
+    addTargetItem: function (text, value) {
+
+        var targetList = document.getElementById(this.id + "lstbxTarget");
+        var targetLen = targetList.options.length;
+
+        targetList[targetLen++] = new Option(text, value);
+
+        targetLen = targetList.options.length;
+        var hidden = $("#" + this.id + "hfTargetValues");
+        var strTarget = '';
+        for (var i = 0; i < targetLen; i++) {
+            if (i < targetLen - 1)
+                strTarget += targetList.options[i].value + "|";
+            else
+                strTarget += targetList.options[i].value
+        }
+
+        hidden.val(strTarget);
+    },
+
+    addSourceToTargetByValue: function (value) {
+        var sourceList = document.getElementById(this.id + "lstbxtSelectionPool");
+        var targetList = document.getElementById(this.id + "lstbxTarget");
+        var sourceLen = sourceList.options.length;
+        var targetLen = targetList.options.length;
+
+        for (var i = sourceLen - 1; i > -1; i--) {           
+            if (sourceList.options[i].value == value) {      
+                targetList[targetLen++] = new Option(sourceList.options[i].text, sourceList.options[i].value);          
+                sourceList.options.remove(i);
+            }
+        }
+
+        targetLen = targetList.options.length;
+        var hidden = $("#" + this.id + "hfTargetValues");
+        var strTarget = '';
+        for (var i = 0; i < targetLen; i++) {
+            if (i < targetLen - 1)
+                strTarget += targetList.options[i].value + "|";
+            else
+                strTarget += targetList.options[i].value
+        }
+
+        hidden.val(strTarget);
+    },
+
+    getTargetValues: function () {
+        return $("#" + this.id + "hfTargetValues").val().split('|');
+    },
+
+    getTargetValueString: function () {
+        return $("#" + this.id + "hfTargetValues").val();
+    },
+
+    clearTargets: function () {
+        $('#' + this.id + 'lstbxTarget').empty();
+    },
+
+    clear: function () {
+        $('#' + this.id + 'lstbxtSelectionPool').empty();
+        $('#' + this.id + 'lstbxTarget').empty();
+    },
+
+    onRemove: function () {
+        return true;
+    },
+
+    onRemoveComplete: function () {
+
+    },
+
+    onAddComplete: function () {
+
+    },
+};
---
+++ Mapp.js
@@ -0,0 +1,87 @@
+﻿
+function initMappings() {
+    resizeMappings();
+
+    $(window).bind('resize', function () {
+        resizeMappings();
+    });
+
+    window.onresize = function (event) {
+        mappingssWindowResize();
+    }
+    window.onresize = function (event) {
+        mappingsTabWindowResize();
+    }
+
+    window.onload = function (e) {
+        resizeMappings();
+    }
+}
+function resizeMappings() {
+    height = calculateMarketMappingContentHeight();
+
+    //there is some margin I cannot find right now
+    //thowing off the height so this hack fixes it
+    height -= 6;
+
+    $("#mappingGeneralContent").height(height);
+
+
+    var width = calculateMarketContentWidth();
+    $("#divMarket").width(width);
+
+    //need to make room for scroll bar
+    $("#mappingGeneralContent").width(width - 15);
+    var gridHeader = 40; // $("tr[id$='dgMarkets_top_head']").css('height');
+    //$("table[id$='dgMarkets']").height(height - gridHeader);
+    $('divMarketMapping').width(width);
+    $('divMarketMapping').height(height - gridHeader);
+    //adjust general tab controls
+    var generalMappingHeader = $("#generalMappingHeader").height();
+
+    $("#mappingGeneralContent").height(height - generalMappingHeader);
+    $("#mappingGeneralContent").width(width + 5);
+
+//    grid = $("table[id$='dgMarkets']");
+//   
+//    grid.width(width + 10);
+//    grid.height(height + 20)
+    //$('#divMarketMapping').height(height - 130);
+
+    //dgMarkets.render();
+
+
+
+}
+function mappingsTabWindowResize() {
+    var height = calculateMarketMappingContentHeight();
+    var width = calculateMarketMappingContentWidth();
+    $('#mappingGeneralContent').width(width + 100);
+    $('#mappingGeneralContent').height(height - 15);
+
+    //    //setTimeout('if( dgProducts ) dgProducts.render();', 100);
+}
+function mappingssWindowResize() {
+   var height = calculateMarketMappingContentHeight();
+   var width = calculateMarketMappingContentWidth();
+   $('#divMarketMapping').width(width - 15);
+   $('#divMarketMapping').height(height - 15);
+
+//    //setTimeout('if( dgProducts ) dgProducts.render();', 100);
+}
+function calculateMarketMappingContentHeight() {
+    var pageHeight = $("html").height();
+    var headerHeight = $(".header").height();
+    var footerHeight = $(".footer").height();
+    var height = pageHeight - 150 - headerHeight - footerHeight;
+    $("#divMarketMapping").height(height);
+    height -= 40; // $("#divMarketHeader").height();
+    return height;
+}
+
+function calculateMarketMappingContentWidth() {
+    var width = $("html").width();
+    width -= $(".SplitterPane").width();
+    width -= $(".HorizontalSplitterBar").width();
+    return $('.SplitterPaneContent').width();
+}
